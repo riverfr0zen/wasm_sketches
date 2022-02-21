766486a50f1051535698cc100aa2d170e6d50ebd
Irfan Baig <riverfr0zen@elitemail.org>
2022-02-20 23:34:18 -0500

    WIP: html file now being generated

--- src/build_sketches.rs
+++ src/build_sketches.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use std::process::{ Command, Stdio };
+use std::fs;
 
 
 #[derive(Parser, Debug)]
@@ -11,13 +12,23 @@ struct Args {
 }
 
 
+fn gen_html_from_template(sketch: &str) {
+    let file_contents = fs::read_to_string(
+        "www/window-matching-canvas.template.html"
+    ).expect("Unable to read file");
+
+    let file_contents = file_contents.replace("{{sketch}}", sketch);
+    fs::write(format!("www/{}.html", sketch), file_contents).expect("Unable to write file");
+}
+
+
 fn build_sketch(sketch: &str) {
     println!("Building {}...", sketch);
     // In the previous commit I was following this example
     // https://rust-lang-nursery.github.io/rust-cookbook/os/external.html#continuously-process-child-process-outputs
     //
     // ...but I couldn't figure out how to get the exit code. Then I found this example
-    // which I think serves this use case better:
+    // using Stdio::inherit which I think serves this use case better:
     // https://stackoverflow.com/a/32020376/4655636
     let mut build_cmd = Command::new("cargo")
         .arg("build")
@@ -31,21 +42,28 @@ fn build_sketch(sketch: &str) {
         .spawn()
         .unwrap();
 
-    let build_success = build_cmd.wait().unwrap().success();
-    if build_success {
-        println!("Running wasm-bindgen for {}...", sketch);
-        Command::new("wasm-bindgen")
-            .arg("--out-dir")
-            .arg("www/wasms")
-            .arg("--target")
-            .arg("web")
-            .arg(format!("target/wasm32-unknown-unknown/release/examples/{}.wasm", sketch))
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .spawn()
-            .unwrap();
-    
+    if ! build_cmd.wait().unwrap().success() {
+        return;
     }
+
+    println!("Running wasm-bindgen for {}...", sketch);
+    let mut wasm_bgen_cmd = Command::new("wasm-bindgen")
+        .arg("--out-dir")
+        .arg("www/wasms")
+        .arg("--target")
+        .arg("web")
+        .arg(format!("target/wasm32-unknown-unknown/release/examples/{}.wasm", sketch))
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()
+        .unwrap();
+
+    if ! wasm_bgen_cmd.wait().unwrap().success() {
+        return;
+    }
+
+    println!("Create html from template");
+    gen_html_from_template(sketch);
 }
 
 
