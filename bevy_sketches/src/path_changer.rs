4c85e6fe0f43c3cf34a2bed2bcb74021d38f3ec5
Irfan Baig <riverfr0zen@elitemail.org>
2022-03-25 11:01:48 -0400

    first pass at shape refinement logic

--- bevy_sketches/src/path_changer.rs
+++ bevy_sketches/src/path_changer.rs
@@ -60,10 +60,56 @@ pub fn path_changer(winsetup: Res<WindowSetup>, mut query: Query<&mut Path>) {
     // @HINT
     // Using an underscore to discard the iterator value since it's not being used
     for _i in 0..num_segments {
-        path_builder.line_to(Vec2::new(
-            rng.gen_range(-winsetup.max_x..winsetup.max_x),
-            rng.gen_range(-winsetup.max_y..winsetup.max_y),
-        ));
+        // if i / num_vertices <= .25:
+        // (next vertex should be: x: range(-maxX, 0), y: range(0, maxY))
+
+        // # top right
+        // if i / num_vertices > .25 and i / num_vertices <= .5:
+        //     (next vertex should be: x: range(0, maxX), y: range(0, maxY))
+
+        // # bottom right
+        // if i / num_vertices > .5 and i / num_vertices <= .75:
+        //     (next vertex should be: x: range(0, maxX), y: range(-maxY, 0))
+
+        // # bottom left
+        // if i / num_vertices > .75 and i / num_vertices <= 1.0:
+        //     (next vertex should be: x: range(-maxX, 0), y: range(-maxY, 0))
+
+        let segment_place: f32 = _i as f32 / num_segments as f32;
+
+        if segment_place <= 0.25 {
+            path_builder.line_to(Vec2::new(
+                rng.gen_range(-winsetup.max_x..0.0),
+                rng.gen_range(0.0..winsetup.max_y),
+            ));
+            continue;
+        }
+
+        if segment_place > 0.25 && segment_place <= 0.5 {
+            path_builder.line_to(Vec2::new(
+                rng.gen_range(0.0..winsetup.max_x),
+                rng.gen_range(0.0..winsetup.max_y),
+            ));
+        }
+
+        if segment_place > 0.5 && segment_place <= 0.75 {
+            path_builder.line_to(Vec2::new(
+                rng.gen_range(0.0..winsetup.max_x),
+                rng.gen_range(-winsetup.max_y..0.0),
+            ));
+        }
+
+        if segment_place > 0.75 && segment_place <= 1.0 {
+            path_builder.line_to(Vec2::new(
+                rng.gen_range(-winsetup.max_x..0.0),
+                rng.gen_range(-winsetup.max_y..0.0),
+            ));
+        }
+
+        // path_builder.line_to(Vec2::new(
+        //     rng.gen_range(-winsetup.max_x..winsetup.max_x),
+        //     rng.gen_range(-winsetup.max_y..winsetup.max_y),
+        // ));
     }
     path_builder.close();
     let new_path = path_builder.build().0;
