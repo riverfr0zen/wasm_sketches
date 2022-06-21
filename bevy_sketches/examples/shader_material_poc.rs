9db7c12026cdfb8e00c1c771f070e9c5fcf2ec33
Irfan Baig <riverfr0zen@elitemail.org>
2022-06-21 13:02:28 -0400

    WIP: spacing

--- bevy_sketches/examples/shader_material_poc.rs
+++ bevy_sketches/examples/shader_material_poc.rs
@@ -11,15 +11,17 @@ use bevy_web_extras::prelude::*;
 
 pub const HEIGHT: f32 = 900.0;
 pub const RESOLUTION: f32 = 16.0 / 9.0;
-const SURFACE_WIDTH: f32 = 500.0;
-const SURFACE_HEIGHT: f32 = 800.0;
+const SURFACE_PADDING: f32 = 0.009;
+// const SURFACE_PADDING: f32 = 0.0;
+const SURFACE_WIDTH: f32 = 0.18;
+const SURFACE_HEIGHT: f32 = 1.0;
 const SURFACE2_WIDTH: f32 = SURFACE_WIDTH;
 // const SURFACE2_HEIGHT: f32 = SURFACE_HEIGHT * 1.5;
 const SURFACE2_HEIGHT: f32 = SURFACE_HEIGHT;
 const SURFACE3_WIDTH: f32 = SURFACE_WIDTH * 1.25;
 const SURFACE3_HEIGHT: f32 = SURFACE_HEIGHT / 2.0;
 const SURFACE4_WIDTH: f32 = SURFACE_WIDTH;
-const SURFACE4_HEIGHT: f32 = SURFACE_HEIGHT / 2.0;
+const SURFACE4_HEIGHT: f32 = SURFACE_HEIGHT;
 
 
 pub fn main() {
@@ -71,6 +73,11 @@ fn handle_post_browser_resize(
 }
 
 
+fn to_scale(webcfg: &Res<WebExtrasCfg>, normalized: Vec2) -> Vec2 {
+    Vec2::new(normalized.x * webcfg.width, normalized.y * webcfg.height)
+}
+
+
 fn poc_setup(
     mut commands: Commands,
     mut entity_q: Query<Entity, With<DisplayQuad>>,
@@ -87,17 +94,20 @@ fn poc_setup(
         commands.entity(entity).despawn();
     }
 
-    // @TODO positioning is messed up on Web because it should be drawn on resize
-    // (see conditional setup of systems in shifty circle)
+    // Don't need padding height, so just passing 1.0
+    let padding_wh = to_scale(&webcfg, Vec2::new(SURFACE_PADDING, 1.0));
+
+    let surface1_wh = to_scale(&webcfg, Vec2::new(SURFACE_WIDTH, SURFACE_HEIGHT));
     commands
         .spawn_bundle(MaterialMesh2dBundle {
             mesh: mesh_assets.add(Mesh::from(shape::Quad::default())).into(),
             transform: Transform {
-                scale: Vec3::new(SURFACE_WIDTH, SURFACE_HEIGHT, 1.0),
+                scale: Vec3::new(surface1_wh.x, surface1_wh.y, 1.0),
+                // translation: Vec3::new(-webcfg.max_x + (surface1_wh.x / 2.0) + 10.0, 0.0, 0.0),
                 translation: Vec3::new(
-                    -webcfg.max_x + (SURFACE_WIDTH / 2.0) + 10.0,
-                    webcfg.max_y - (SURFACE_HEIGHT / 2.0),
-                    1.0,
+                    -webcfg.max_x + (surface1_wh.x / 2.0) + padding_wh.x,
+                    0.0,
+                    0.0,
                 ),
                 ..Transform::default()
             },
@@ -106,15 +116,17 @@ fn poc_setup(
         })
         .insert(DisplayQuad);
 
+    let surface2_wh = to_scale(&webcfg, Vec2::new(SURFACE2_WIDTH, SURFACE2_HEIGHT));
     commands
         .spawn_bundle(MaterialMesh2dBundle {
             mesh: mesh_assets.add(Mesh::from(shape::Quad::default())).into(),
             transform: Transform {
-                scale: Vec3::new(SURFACE_WIDTH, SURFACE2_HEIGHT, 1.0),
+                scale: Vec3::new(surface2_wh.x, surface2_wh.y, 1.0),
                 translation: Vec3::new(
-                    -webcfg.max_x + (SURFACE2_WIDTH / 2.0) + SURFACE_WIDTH + 20.0,
-                    webcfg.max_y - (SURFACE2_HEIGHT / 2.0),
-                    1.0,
+                    // -webcfg.max_x + (surface2_wh.x / 2.0) + surface1_wh.x + 20.0,
+                    -webcfg.max_x + (surface2_wh.x / 2.0) + surface1_wh.x + (padding_wh.x * 2.0),
+                    0.0,
+                    0.0,
                 ),
                 ..Default::default()
             },
@@ -123,15 +135,21 @@ fn poc_setup(
         })
         .insert(DisplayQuad);
 
+    let surface3_wh = to_scale(&webcfg, Vec2::new(SURFACE3_WIDTH, SURFACE3_HEIGHT));
     commands
         .spawn_bundle(MaterialMesh2dBundle {
             mesh: mesh_assets.add(Mesh::from(shape::Quad::default())).into(),
             transform: Transform {
-                scale: Vec3::new(SURFACE3_WIDTH, SURFACE3_HEIGHT, 1.0),
+                scale: Vec3::new(surface3_wh.x, surface3_wh.y, 1.0),
                 translation: Vec3::new(
-                    -webcfg.max_x + (SURFACE3_WIDTH / 2.0) + SURFACE_WIDTH + SURFACE2_WIDTH + 30.0,
-                    webcfg.max_y - (SURFACE3_HEIGHT / 2.0),
-                    1.0,
+                    // -webcfg.max_x + (surface3_wh.x / 2.0) + surface1_wh.x + surface2_wh.x + 30.0,
+                    -webcfg.max_x
+                        + (surface3_wh.x / 2.0)
+                        + surface1_wh.x
+                        + surface2_wh.x
+                        + (padding_wh.x * 3.0),
+                    0.0,
+                    0.0,
                 ),
                 ..Default::default()
             },
@@ -140,21 +158,22 @@ fn poc_setup(
         })
         .insert(DisplayQuad);
 
-
+    let surface4_wh = to_scale(&webcfg, Vec2::new(SURFACE4_WIDTH, SURFACE4_HEIGHT));
     commands
         .spawn_bundle(MaterialMesh2dBundle {
             mesh: mesh_assets.add(Mesh::from(shape::Quad::default())).into(),
             transform: Transform {
-                scale: Vec3::new(SURFACE4_WIDTH, SURFACE4_HEIGHT, 1.0),
+                scale: Vec3::new(surface4_wh.x, surface4_wh.y, 1.0),
                 translation: Vec3::new(
                     -webcfg.max_x
-                        + (SURFACE4_WIDTH / 2.0)
-                        + SURFACE_WIDTH
-                        + SURFACE2_WIDTH
-                        + SURFACE3_WIDTH
-                        + 40.0,
-                    webcfg.max_y - (SURFACE4_HEIGHT / 2.0),
-                    1.0,
+                        + (surface4_wh.x / 2.0)
+                        + surface1_wh.x
+                        + surface2_wh.x
+                        + surface3_wh.x
+                        // + 40.0,
+                        + (padding_wh.x * 4.0),
+                    0.0,
+                    0.0,
                 ),
                 ..Default::default()
             },
@@ -163,21 +182,23 @@ fn poc_setup(
         })
         .insert(DisplayQuad);
 
-
+    let surface5_wh = to_scale(&webcfg, Vec2::new(SURFACE4_WIDTH, SURFACE4_HEIGHT));
     commands
         .spawn_bundle(MaterialMesh2dBundle {
             mesh: mesh_assets.add(Mesh::from(shape::Quad::default())).into(),
             transform: Transform {
-                scale: Vec3::new(SURFACE4_WIDTH, SURFACE4_HEIGHT - 10.0, 1.0),
+                scale: Vec3::new(surface5_wh.x, surface5_wh.y, 1.0),
                 translation: Vec3::new(
                     -webcfg.max_x
-                        + (SURFACE4_WIDTH / 2.0)
-                        + SURFACE_WIDTH
-                        + SURFACE2_WIDTH
-                        + SURFACE3_WIDTH
-                        + 40.0,
-                    webcfg.max_y - (SURFACE4_HEIGHT / 2.0) - SURFACE4_HEIGHT,
-                    1.0,
+                        + (surface5_wh.x / 2.0)
+                        + surface1_wh.x
+                        + surface2_wh.x
+                        + surface3_wh.x
+                        + surface4_wh.x
+                        // + 40.0,
+                        + (padding_wh.x * 5.0),
+                    0.0,
+                    0.0,
                 ),
                 ..Default::default()
             },
